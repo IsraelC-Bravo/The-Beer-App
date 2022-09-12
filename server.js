8b3af31ddbeb088b769cbcd3114a700d933862f1
Israel Castillo <israelocastillo@gmail.com>
2022-09-11 21:26:01 -0500

    added express modules

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 2121
+require('dotenv').config()
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'The beer App'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
